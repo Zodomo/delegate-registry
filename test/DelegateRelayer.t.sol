cd2a738de881e4c77e0a08cde6117e34c834adfd
Zodomo <zodomo@proton.me>
2023-11-01 23:02:24 -0500

    initial tests and beginning of LZ testing

--- test/DelegateRelayer.t.sol
+++ test/DelegateRelayer.t.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.21;
 
 import {Test} from "forge-std/Test.sol";
 import {RegistryHarness} from "./tools/RegistryHarness.sol";
-import {IDelegateRegistry} from "src/IDelegateRegistry.sol";
+import {RegistryData as Data}  from "../src/libraries/RegistryData.sol";
 import {LZEndpointMock} from "solidity-examples/lzApp/mocks/LZEndpointMock.sol";
+import {BytesLib} from "solidity-bytes-utils/BytesLib.sol";
 
 contract DelegateRegistryTest is Test {
     LZEndpointMock public lzEndpointA;
@@ -12,6 +13,7 @@ contract DelegateRegistryTest is Test {
     RegistryHarness public regA;
     RegistryHarness public regB;
     bytes32 public rights = "";
+    bytes public params = "";
 
     function setUp() public {
         lzEndpointA = new LZEndpointMock(1);
@@ -22,5 +24,47 @@ contract DelegateRegistryTest is Test {
         regB.setRemoteReg(address(regA));
         lzEndpointA.setDestLzEndpoint(address(regB), address(lzEndpointB));
         lzEndpointB.setDestLzEndpoint(address(regA), address(lzEndpointA));
+    }
+
+    function regAGetPayloadAll() public view returns (bytes memory payload) {
+        payload = regA.getPayload(
+            Data.DelegationType.ALL,
+            true,
+            address(22222),
+            address(11111),
+            address(0),
+            0,
+            0,
+            rights
+        );
+    }
+
+    function testRegAGetPayloadAll() public view returns (bytes memory payload) {
+        payload = regAGetPayloadAll();
+        require(!BytesLib.equal(payload, (bytes(""))), "payload generation failed");
+    }
+
+    function testRegAEstimateFees() public view returns (uint256 nativeFee, uint256 zroFee) {
+        (nativeFee, zroFee) = regA.estimateFees(2, regAGetPayloadAll(), false, params);
+    }
+
+    function testRegADelegateAll() public returns (bytes32 hash) {
+        uint16[] memory dstChainIds = new uint16[](1);
+        dstChainIds[0] = 2;
+        uint[] memory nativeFees = new uint[](1);
+        (nativeFees[0], ) = testRegAEstimateFees();
+        bytes[] memory adapterParams = new bytes[](1);
+        adapterParams[0] = params;
+        vm.deal(address(11111), 10 ether);
+        vm.prank(address(11111));
+        hash = regA.delegateAll{ value: nativeFees[0] }(
+            address(22222),
+            rights,
+            true,
+            dstChainIds,
+            address(0),
+            nativeFees,
+            adapterParams
+        );
     }
 }
